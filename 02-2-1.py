2001e3d54fb292b38a8ab111357d9ed14e6388f2
JunhyunJunhyun <sji010900@gmail.com>
2020-08-22 21:31:54 +0900

    첨삭

---
+++ 02-2-1.py
@@ -0,0 +1,11 @@
+a = "Hello World"
+print(a)
+
+a = "Hello World"
+print(a)
+
+a = """Life is too short, You need python"""
+print(a)
+
+a = """Life is too short, You need python"""
+print(a)
---
+++ 02-2-2.py
@@ -0,0 +1,10 @@
+food = "Python's favorite food is perl"
+print(food)
+
+say = '"Python is very easy." he says.'
+print(say)
+
+food = "Python's favorite food is perl"
+say = '"Python is very easy." he says.'
+print(food)
+print(say)
