b979b06c7902dc68a96ae6d3276908c5bfe68c40
sungchan1120 <yuyiyo1000@gmail.com>
2020-09-12 11:56:52 +0900

    kk

--- Q/03-2.py
+++ Q/03-2.py
@@ -18,14 +18,39 @@
 #     print(prompt)
 #     number = int(input())
 
-coffee = 10
-money = 300
+# coffee = 10
+# money = 300
 
-while money:
-    print("돈을 받았으니 커피를 줍니다")
-    coffee = coffee - 1
-    print("남은 커피의 양은 %d개입니다." % coffee)
+# while money:
+#     print("돈을 받았으니 커피를 줍니다")
+#     coffee = coffee - 1
+#     print("남은 커피의 양은 %d개입니다." % coffee)
 
-    if coffee == 0:
-        print("커피가 다 떨어졌습니다. 판매를 중지합니다")
-        break
+#     if coffee == 0:
+#         print("커피가 다 떨어졌습니다. 판매를 중지합니다")
+#         break
+
+# coffee = 10
+# while True:
+#     money = int(input("돈을 넣어 주세요:"))
+#     if money == 300:
+#         print("커피를 줍니다")
+#         coffee = coffee -1
+#     elif money > 300:
+#         print("거스름돈 %d를 주고 커피를 줍니다." %(money -3000))
+#         coffee = coffee -1
+#     else:
+#         print("돈을 다시 돌려주고 커피를 주지 않습니다.")
+#         print("남은 커피의 양은 %d개 입니다." % coffee)
+#     if coffee == 0:
+#         print("커피가 다 떨어졌습니다. 판매를 중지 합니다.")
+#         break
+
+# a = 0
+# while a < 10:
+#     a = a + 1
+#     if a % 2 == 0: continue
+#     print(a)
+
+while True:
+    print("ctrl=c를 눌러야 whlie문을 빠져나갈 수 있습니다")
