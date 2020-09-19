9c1f7545deb9a669b692aeb751a17cf6cd3db156
sungchan1120 <yuyiyo1000@gmail.com>
2020-09-19 10:00:04 +0900

    assignment

---
+++ Q/q3-2.py
@@ -0,0 +1,28 @@
+a = 0
+result = 0
+while True:
+    a += 3
+    if a>1000:break
+    result += a
+
+print(result)
+
+a = 0
+result = 0
+while True:
+    a += 3,5
+    if a>1000:Bleak
+    result += a
+
+print(result)
+
+a = 0
+result = 0
+while True:
+    if a % 3 == 0 and a % 5 == 0:
+        a += 3
+    result += a
+    if a>1000:break
+    
+print(result)
+     
