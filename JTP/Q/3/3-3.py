4cb9f35d3197d14f1bf2c7c4bd9b5b0dba29d86c
sungchan1120 <yuyiyo1000@gmail.com>
2020-09-19 11:03:51 +0900

    aa

--- JTP/Q/3/3-3.py
+++ JTP/Q/3/3-3.py
@@ -16,6 +16,20 @@ while True:
 
 
 
- *
-***
- *
+
+i = 0
+while True:
+    i += 1
+    print(i * "*")
+
+    if i > 0:
+        break
+
+i = 0
+while True:
+    i += 1
+    print(1 * "*")
+
+    if i > 2:
+        break
+
