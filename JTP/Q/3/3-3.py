966339ca9f8639006de8a716297714040783d0a1
JunhyunJunhyun <sji010900@gmail.com>
2020-09-22 12:34:37 +0900

    첨삭

--- JTP/Q/3/3-3.py
+++ JTP/Q/3/3-3.py
@@ -1,41 +1,43 @@
-i = 0
+# i = 0
 
-while True:
-    i += 1
-    print(i * "*")
+# while True:
+#     i += 1
+#     print(i * "*")
 
-    if i > 4:
-        break
+#     if i > 4:
+#         break
 
-while True:
-    i -= 1
-    print(i * "*")
+# while True:
+#     i -= 1
+#     print(i * "*")
 
-    if i == 0:
-        break
+#     if i == 0:
+#         break
 
-i = 0
-while True:
-    i += 1
-    print(1 * " * ") 
-    break
+# // while_count >> 3
+#  *      // b1 s1
+# ***     // b0 s3
+#  *      // b1 s1
 
-i = 0
-while True:
-    i += 1
-    print(3 * "*")
-    break
+# // while_count >> 5
+#   *     // b2 s1
+#  ***    // b1 s3
+# *****   // b0 s5
+#  ***    // b1 s3
+#   *     // b2 s1
 
-i = 0
-while True:
-    print(1 * " * ")
-    break
+# // while_count >> 7
+#    *
+#   ***
+#  *****
+# *******
+#  *****
+#   ***
+#    *
 
-i = 0
-while True:
-    i += 1
-    print(1 * "*", 3 * "*", 1 *"*")
-    break
-    
+n = int(input("input : "))
+for i in range(1, n + 1):  # range(1, 6) => 1, 2, 3, 4, 5
+    print(" " * (n - i), "*" * (2 * i - 1))
 
-
+# 공백 => 5-1, 5-2, 5-3, 5-4, 5-5
+# 별 => 2*1-1, 2*2-1, 2*3-1, 2*4-1, 2*5-1
