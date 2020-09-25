f8fe95398500a9e406813080c5f7929376cabd3c
JunhyunJunhyun <sji010900@gmail.com>
2020-09-25 18:20:28 +0900

    첨삭

---
+++ JTP/abc.py
@@ -0,0 +1,30 @@
+# 1
+# a = 80
+# b = 75
+# c = 55
+# print((a + b + c) /3)
+
+# 2
+# if 13 % 2 == 1:
+#     print("홀수")
+# else:
+#     print("짝수")
+
+# 3
+# a = "881120-1068234"
+# print(a[:6])
+# print(a[7:])
+
+# 4
+# a = "881120-1068234"
+# print(a[7])
+
+# 5
+# a = "a:b:c:d"
+# a = a.replace(":", "#")
+# print(a)
+
+# 6
+# a = [1, 3, 5, 4, 2]
+# a.sort(reverse=True)
+# print(a)
