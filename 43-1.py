c7bbbc894918831505cdb821164236acf6dcda62
JunhyunJunhyun <sji010900@gmail.com>
2020-10-23 09:59:12 +0900

    첨삭

--- 43-1.py
+++ 43-1.py
@@ -6,17 +6,19 @@
 
 # def say():
 #     return "가""위"
-# a = say 
+# a = say
 # print(a)
 
-# def is_odd(number):
-#     if number % 2 == 1:
-#         return True
-#     else:
-#         return False
 
-input1 = input("첫 번째 숫자를 입력하세요:")
-input2 = input("두 번째 숫자를 입력하세요")
+def is_odd(number):
+    if number % 2 == 1:
+        return True
+    else:
+        return False
 
-total = int(input1) + int(input2)
-print("두수의 합은 %s 입니다" % total)
+
+# input1 = input("첫 번째 숫자를 입력하세요:")
+# input2 = input("두 번째 숫자를 입력하세요:")
+
+# total = int(input1) + int(input2)
+# print("두수의 합은 %s 입니다" % total)
