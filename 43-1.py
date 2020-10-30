609cade4762c4fcc1f1740274723ecc6458c2cc7
sungchan1120 <yuyiyo1000@gmail.com>
2020-10-30 09:49:21 +0900

    no

--- 43-1.py
+++ 43-1.py
@@ -4,15 +4,15 @@
 #         print(n)
 
 
-def say(*args):
-    result = ""
-    for a in args:
-        result += a
+# def say(*args):
+#     result = ""
+#     for a in args:
+#         result += a
 
-    print(result)
+#     print(result)
 
 
-say("text1", "text2", "text3")
+# say("text1", "text2", "text3")
 
 
 # say("가", "위")
@@ -33,3 +33,26 @@ say("text1", "text2", "text3")
 
 # total = int(input1) + int(input2)
 # print("두수의 합은 %s 입니다" % total)
+
+# print("you" "need" "python")
+# print("you"+"need"+"python")
+# print("you","need","python")
+# print("".join(["you","need","python"]))
+
+# f1 = open("test.txt", 'w')
+# f1.write("Life is too short!")
+# f1.close() 
+
+# f2 = open("test.txt", 'r')
+# print(f2.read())
+# f2.close()
+def avg_number(*args):
+    result = 0
+    for i in args:
+        result += i
+    return result len(*args)
+
+avg_number(1,2)
+
+
+
