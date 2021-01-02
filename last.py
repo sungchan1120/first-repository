1794fb89d3e9ff4cf10f8514eb467e5bf72346ab
andychoi <andychoi@andychoiui-MacBookPro.local>
2021-01-02 21:18:15 +0900

    f

---
+++ last.py
@@ -0,0 +1,28 @@
+# a = "a:b:c:d"
+# b = a.split(":")
+# c = "#".join(b)
+# print(c)
+
+# a = {'a':90, 'b':80}
+# a.gat('c',70)
+# print (a)
+
+a = [20, 55, 67, 82, 45, 33, 90, 87, 100, 25]
+result = 0
+while a:
+    mark = a.pop()
+    if mark >= 50:
+        result += mark
+print(result)
+
+print(65+45+2+3+45+8)
+
+print(':: 2단 ::')
+for i in range(1, 10):  # 1부터 9까지
+    print(f'2 x {i} = {2*i}')
+
+
+print((70+60+55+75+95+90+80+80+85+100)/10)
+
+
+
--- ttt.py
+++ ttt.py
@@ -12,7 +12,7 @@
 #     for i in l: 
 #         if i > 0: 
 #             result.append(i) 
-#     return result
+#     return reslt
 
 # print(positive([1,-3,2,0,-5,6]))
 # print(hex(234))
