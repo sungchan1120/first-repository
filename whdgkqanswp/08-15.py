582e0e8026f8d36fd3f03795559520cf7ab34864
andychoi <andychoi@andychoiui-MacBookPro.local>
2021-01-20 20:23:16 +0900

    f

--- whdgkqanswp/08-15.py
+++ whdgkqanswp/08-15.py
@@ -1,2 +1,2 @@
 a = input("0~9 사이의 숫자로 이루어진 문자열 입력 : ")
-print('true' if len(a) == len(set(a)) == 10  else 'false')
+print('true' if len(a) == len(set(a)) == 10  else 'false')#0~9까지한번씩만 사용하면true,아님 false
