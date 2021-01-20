ecc809cc0eed67219b85328c11b0de404d014fbc
repl.it user <replituser@example.com>
2021-01-20 11:37:00 +0000

    T Commit

--- whdgkqanswp/08-19.py
+++ whdgkqanswp/08-19.py
@@ -1,8 +1,14 @@
 import re 
 
 data = """
-park 010-9988-9999
+park 010-9999-9988
+kim 010-9909-7789
+lee 010-8789-7768
 """
 
-pat = re.compile("(\d{3})[-](\d{4})[-](\d{4})")
-print(pat.sub("\g<3>-####", data))
+pat = re.compile("(\d{3}[-]\d{4})[-]\d{4}")
+pat = re.compile("(\d{3}[-]\d{4})[-]\d{4}")
+print(pat.sub("\g<1>-####", data))
+result = pat.sub("\g<1>-####", data)
+
+print(result)
